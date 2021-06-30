bdb41102e0d770ed477fc4a6ca3350f9abd4cc8f
Muhammad <85280384+Hama-Aryan@users.noreply.github.com>
2021-06-30 15:03:23 +0300

    Programm to shut down computer
    bo ferbuny kuzhandnaway computer

---
+++ Shut down computer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shat_down
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Computerakat dakuzhenitawa?", "tkaya walam bdawa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(result == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+            }
+        }
+    }
+}
